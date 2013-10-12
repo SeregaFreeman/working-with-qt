91645ddebc754b1a4695c1512aeb715a48d8f3d5
SeregaFreeman <gordonfrimen94@gmail.com>
2013-10-12 18:55:53 +0300

    trying out qt1

--- widget.cpp
+++ widget.cpp
@@ -1,11 +1,33 @@
 #include "widget.h"
 
 Widget::Widget(QWidget *parent)
-    : QWidget(parent)
+    : QWidget(parent),
+    slide(new QSlider()),
+    spin(new QSpinBox()),
+    lcd(new QLCDNumber()),
+    h_layout(new QHBoxLayout()),
+    v_layout(new QVBoxLayout())
 {
+    setLayout(v_layout);
+    v_layout->addLayout(h_layout);
+    //v_layout->addWidget(line);
+    h_layout->addWidget(slide);
+    h_layout->addWidget(spin);
+    //h_layout->addWidget(yes);
+    //h_layout->addWidget(no);
+    h_layout->addWidget(lcd);
+    //mpr->setMapping(yes, "You choose yes");
+    //mpr->setMapping(no,  "You choose no");
+    connect(spin, SIGNAL(valueChanged(int)),slide, SLOT(setValue(int)));
+    connect(slide, SIGNAL(valueChanged(int)),spin, SLOT(setValue(int)));
+    connect(spin, SIGNAL(valueChanged(int)),lcd, SLOT(display(int)));
+    //connect(yes, SIGNAL(clicked()), mpr, SLOT(map()));
+    //connect(no, SIGNAL(clicked()), mpr, SLOT(map()));
+
 }
 
 Widget::~Widget()
 {
-    
+
 }
+
--- widget.h
+++ widget.h
@@ -1,15 +1,26 @@
 #ifndef WIDGET_H
 #define WIDGET_H
-
 #include <QWidget>
+#include <QtWidgets>
 
 class Widget : public QWidget
 {
     Q_OBJECT
-    
+
 public:
     Widget(QWidget *parent = 0);
     ~Widget();
+
+
+private:
+    QSlider *slide;
+    QSpinBox *spin;
+    QLCDNumber *lcd;
+    QHBoxLayout *h_layout;
+    QVBoxLayout *v_layout;
+    QPushButton *yes, *no;
+    QLineEdit *line;
+
 };
 
 #endif // WIDGET_H
